4c730836a2c57882ccf285a7d68e1b2f50703604
ehe759 <ehe759@student.fuhsd.org>
2025-09-27 16:28:20 -0700

    getting rid of excess prints

--- dataset.py
+++ dataset.py
@@ -167,7 +167,6 @@ class CustomDataset(Dataset):
         list_samples = []
         list_indices = [[1,2,4], [1,3,4], [2,3,5], [2,4,5], [3,4,6], [3,5,6], [4,5,7], [4,6,7]]
         for i in range(8):
-            print(list_indices[i])
             img0, gt, img1, timestep = self.getimg(index, list_indices[i][0], list_indices[i][1], list_indices[i][2])
             if self.dataset_name == 'train':
                 img0, gt, img1 = self.crop(img0, gt, img1, 224, 224)
